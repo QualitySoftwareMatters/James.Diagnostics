e9f7706d959e71b07c95fa928e14523c6a81f368
Todd Meinershagen <todd@meinershagen.net>
2015-07-05 17:56:33 -0500

    Fixing the nuspec file - bad characters.

--- src/James.Diagnostics/Properties/AssemblyInfo.cs
+++ src/James.Diagnostics/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Todd Meinershagen")]
 [assembly: AssemblyProduct("James.Diagnostics")]
-[assembly: AssemblyCopyright("Copyright © QualitySoftwareMatters 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
