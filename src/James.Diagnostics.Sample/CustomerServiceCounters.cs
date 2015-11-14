eeeb1e3151d2d7496d3d6ebfbce79fb00e185398
Todd Meinershagen <todd@meinershagen.net>
2015-11-13 23:17:20 -0600

    Updated the sample project.

--- src/James.Diagnostics.Sample/CustomerServiceCounters.cs
+++ src/James.Diagnostics.Sample/CustomerServiceCounters.cs
@@ -1,11 +1,5 @@
-﻿using Magnum.PerformanceCounters;
-
-namespace James.Diagnostics.Sample
+﻿namespace James.Diagnostics.Sample
 {
-	public class CustomerServiceCounters : CounterCategory, IMonitorableCounterCategory
-	{
-		public Counter Succeeded { get; set; }
-		public Counter Failed { get; set; }
-		public Counter ExecutionTime { get; set; }
-	}
+	public class CustomerServiceCounters : MonitorableCounterCategory
+	{}
 }
